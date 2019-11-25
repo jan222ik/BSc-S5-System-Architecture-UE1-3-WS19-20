fa764a286f0291a119b5c0cd7e10a04bf3c6dc2d
Matthias Rupp <mru3383@students.fhv.at>
2019-11-25 11:56:24 +0100

    Changed pom to be able to package Beans

--- componentbeans/src/main/java/component/beans/filter/ErodeFilterBean.java
+++ componentbeans/src/main/java/component/beans/filter/ErodeFilterBean.java
@@ -1,10 +1,10 @@
 package component.beans.filter;
 
-import component.beans.dataobj.ImgDTO;
+/*import component.beans.dataobj.ImgDTO;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Imgproc;*/
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -32,12 +32,12 @@ public class ErodeFilterBean implements Serializable {
         mPcs.removePropertyChangeListener(listener);
     }
 
-    private void process(ImgDTO entity) {
+  /*  private void process(ImgDTO entity) {
         Mat element = Imgproc.getStructuringElement(shapeType, new Size(2 * kernalSize + 1, 2 * kernalSize + 1), new Point(kernalSize, kernalSize));
         Mat dst = new Mat();
         Imgproc.erode(entity.getMat(), dst, element);
         entity.setMat(dst);
-    }
+    }*/
 
     public int getShapeType() {
         return shapeType;

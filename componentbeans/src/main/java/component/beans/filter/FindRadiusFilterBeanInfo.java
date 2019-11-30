c0c52aaa82dea7b42873089982ce1271191fdc0a
Matthias Rupp <mru3383@students.fhv.at>
2019-11-30 14:23:10 +0100

    Added BeanInfo-classes for all Beans

--- componentbeans/src/main/java/component/beans/filter/FindRadiusFilterBeanInfo.java
+++ componentbeans/src/main/java/component/beans/filter/FindRadiusFilterBeanInfo.java
@@ -1,6 +1,35 @@
 package component.beans.filter;
 
+import java.beans.IntrospectionException;
+import java.beans.MethodDescriptor;
+import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
 public class FindRadiusFilterBeanInfo extends SimpleBeanInfo {
+
+    private static final Class beanClass;
+
+    static {
+        beanClass = FindRadiusFilter.class;
+    }
+
+    public FindRadiusFilterBeanInfo() {
+    }
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        try {
+            PropertyDescriptor threshold = new PropertyDescriptor("threshold", beanClass);
+            threshold.setBound(true);
+            return new PropertyDescriptor[]{threshold};
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+            return new PropertyDescriptor[]{};
+        }
+    }
+
+    @Override
+    public MethodDescriptor[] getMethodDescriptors() {
+        return null;
+    }
 }
--- componentbeans/src/main/java/component/beans/filter/MedianFilterBeanInfo.java
+++ componentbeans/src/main/java/component/beans/filter/MedianFilterBeanInfo.java
@@ -1,6 +1,34 @@
 package component.beans.filter;
 
+import java.beans.IntrospectionException;
+import java.beans.MethodDescriptor;
+import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
 public class MedianFilterBeanInfo extends SimpleBeanInfo {
+    private static final Class beanClass;
+
+    static {
+        beanClass = MedianFilter.class;
+    }
+
+    public MedianFilterBeanInfo() {
+    }
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        try {
+            PropertyDescriptor ksize = new PropertyDescriptor("ksize", beanClass);
+            ksize.setBound(true);
+            return new PropertyDescriptor[]{ksize};
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+            return new PropertyDescriptor[]{};
+        }
+    }
+
+    @Override
+    public MethodDescriptor[] getMethodDescriptors() {
+        return null;
+    }
 }
